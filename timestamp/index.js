8dd5b09d5a80cf39b01d850f0b7cd5b5601301d3
Leonel Higuera <lhiguer1@asu.edu>
2023-02-11 06:02:39 -0700

    timestamp

---
+++ timestamp/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+const path = require('path');
+var router = express.Router();
+
+
+router.use('/static/timestamp/css', express.static(path.join(__dirname,'static', 'timestamp', 'css')));
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+router.get('/api/:date?', (req, res) => {
+    let reqDate = req.params.date
+
+    let date;
+
+    if (!reqDate) {
+        date = new Date();
+    } else {
+        date = Date.parse(reqDate) ? new Date(reqDate) : new Date(Number(reqDate));
+    }
+
+    if (isNaN(date)) {
+        res.json({ error : "Invalid Date" });
+        return;
+    }
+
+    res.send({
+        utc: date.toUTCString(),
+        unix: Number(date),
+    });
+});
+
+module.exports = router;
