d6f3add63cc97a5f51ec0970acd0d71921264c45
Leonel Higuera <lhiguer1@asu.edu>
2023-02-16 18:50:39 -0700

    add cors and other misc changes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
-var timestampRouter = require('./timestamp')
-var headerParserRouter = require('./headerparser')
+var timestampRouter = require('./timestamp');
+var headerParserRouter = require('./headerparser');
 
 var app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(logger('dev'));
 app.use(express.json());
